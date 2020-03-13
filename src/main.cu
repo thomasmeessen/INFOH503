75e2e4c8d52c0752b595a7912e39952d2c46f134
lisa.ulb.camera <lisa.ulb.camera@gmail.com>
2020-03-13 14:23:53 +0100

    Hello World with CMake and CUDA

---
+++ src/main.cu
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+__global__ 
+void cuda_hello() {
+	printf("Hello World from GPU!\n");
+}
+
+int main() {
+	cuda_hello << <1, 1 >> > ();
+
+
+
+	do {
+		
+	} while (1);
+
+	return 0;
+}
