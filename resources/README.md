eebc7f6985ca5f1767ebe769e625a3a438681b3e
tmeessen <meessen.thomas@gmail.com>
2020-04-04 11:35:57 +0200

    Adding 3000 x 2000 test images

---
+++ resources/README.md
@@ -0,0 +1,6 @@
+# Dataset Source
+
+Website with excellent dataset :
+
+http://vision.middlebury.edu/stereo/data/scenes2014/
+
--- src/main.cpp
+++ src/main.cpp
@@ -56,7 +56,7 @@ int main(int argc, char ** argv)
     cl_kernel kernel = clCreateKernel( program, "memset", NULL );
 
     // 5. Load an image into a buffer
-    cv::Mat source_image = cv::imread("paper0.png", cv::IMREAD_COLOR);
+    cv::Mat source_image = cv::imread("classroom_l.png", cv::IMREAD_COLOR);
     source_image.convertTo(source_image, CV_8U);  // for greyscale
 
     cv::imwrite("pre.png", source_image);
