4740268fd4a1fc74b05dce4655c4f0f790d1dc4c
Piotr Rochala <piotrek2236@hotmail.com>
2020-04-11 16:18:23 +0200

    elem size changed

--- src/main.cpp
+++ src/main.cpp
@@ -16,22 +16,22 @@ using namespace std;
 const string greyscale_source_path = "greyscale.cl";
 const string difference_image_source_path = "differenceImage.cl";
 const string guidedFilter_source_path = "guidedFilter.cl";
-const string left_image_path  = "classroom_l.png";
+const string left_image_path = "classroom_l.png";
 const string right_image_path = "classroom_r.png";
 
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
     // 1. Get a platform.
     cl_platform_id platform;
-    clGetPlatformIDs( 1, &platform, NULL );
+    clGetPlatformIDs(1, &platform, NULL);
 
     // 2. Find a gpu device.
     cl_device_id device;
-    clGetDeviceIDs( platform,
-                    CL_DEVICE_TYPE_GPU,
-                    1,
-                    &device, NULL);
+    clGetDeviceIDs(platform,
+        CL_DEVICE_TYPE_GPU,
+        1,
+        &device, NULL);
 
     // 2.1 Check if the device tolerate images
     //cl_bool param_value_image_accepted;
@@ -39,47 +39,46 @@ int main(int argc, char ** argv)
     //cout <<( (param_value_image_accepted == CL_TRUE)? "Device accept image ": "Device do not accept image") << endl;
 
     // 3. Create a context and command queue on that device.
-    cl_context context = clCreateContext( NULL,
-                                          1,
-                                          &device,
-                                          NULL, NULL, NULL);
+    cl_context context = clCreateContext(NULL,
+        1,
+        &device,
+        NULL, NULL, NULL);
 
-    cl_command_queue queue = clCreateCommandQueue( context,
-                                                   device,
-                                                   0, NULL );
-   
+    cl_command_queue queue = clCreateCommandQueue(context,
+        device,
+        0, NULL);
+
     cl_program greyscale_program;
     compile_source(&greyscale_source_path, &greyscale_program, device, context);
-    cl_kernel greyscale_kernel = clCreateKernel( greyscale_program, "memset", NULL );
+    cl_kernel greyscale_kernel = clCreateKernel(greyscale_program, "memset", NULL);
 
     cl_program guidedFilter_program;
     compile_source(&greyscale_source_path, &guidedFilter_program, device, context);
-    cl_kernel guidedFilter_kernel = clCreateKernel( guidedFilter_program, "memset", NULL );
+    cl_kernel guidedFilter_kernel = clCreateKernel(guidedFilter_program, "memset", NULL);
 
     cv::Mat left_image;
-    cv::Mat left_image_gray =  to_greyscale_plus_padding(left_image_path, left_image, MAX_DISTANCE ,context, greyscale_kernel, queue, false);
+    to_greyscale_plus_padding(&left_image_path, left_image, MAX_DISTANCE, context, greyscale_kernel, queue, false);
     cv::Mat right_image;
-    cv::Mat right_image_gray = to_greyscale_plus_padding(right_image_path, right_image, MAX_DISTANCE, context, greyscale_kernel, queue, true);
+    to_greyscale_plus_padding(&right_image_path, right_image, MAX_DISTANCE, context, greyscale_kernel, queue, false);
 
-    //guidedFilter(left_image_path ,left_image_gray, context, guidedFilter_kernel, queue, false);
-    //guidedFilter(right_image_path ,right_image_gray, context, guidedFilter_kernel, queue, false);
+    guidedFilter(&left_image_path, left_image, context, guidedFilter_kernel, queue, true);
 
 
-   // cv::Mat right_image;
-    //to_greyscale(right_image_path, right_image, context, greyscale_kernel, queue, false);
+    // cv::Mat right_image;
+     //to_greyscale(right_image_path, right_image, context, greyscale_kernel, queue, false);
 
-//--------------------------------------------
-//--------Difference Image Kernel-------------
-//--------------------------------------------
-    //now source image == the greysclae image
+ //--------------------------------------------
+ //--------Difference Image Kernel-------------
+ //--------------------------------------------
+     //now source image == the greysclae image
 
     cl_program difference_image_program;
     compile_source(&difference_image_source_path, &difference_image_program, device, context);
 
-    cl_kernel difference_image_kernel = clCreateKernel( difference_image_program, "memset", NULL );
+    cl_kernel difference_image_kernel = clCreateKernel(difference_image_program, "memset", NULL);
 
     cv::Mat output_image; // each image will be next to each other?
-    image_difference(left_image_gray, right_image_gray, output_image, MAX_DISTANCE, context, difference_image_kernel, queue, true);
+    image_difference(left_image, right_image, output_image, MAX_DISTANCE, context, difference_image_kernel, queue, true);
 
     left_image.release();
     right_image.release();
