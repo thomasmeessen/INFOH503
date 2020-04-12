9235db929e3864244ca0118e9e8ec06c7adab237
tmeessen <meessen.thomas@gmail.com>
2020-04-12 16:15:44 +0200

    Cleaning for demo_branch

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,6 @@ int main(int argc, char** argv)
         1,
         &device, NULL);
 
-    // 2.1 Check if the device tolerate images
-    //cl_bool param_value_image_accepted;
-    //clGetDeviceInfo(device, CL_DEVICE_IMAGE_SUPPORT, sizeof(cl_bool), (void *)param_value_image_accepted, NULL);
-    //cout <<( (param_value_image_accepted == CL_TRUE)? "Device accept image ": "Device do not accept image") << endl;
-
 
     // 3. Create a context and command queue on that device.
     cl_context context = clCreateContext(NULL,
@@ -50,91 +45,21 @@ int main(int argc, char** argv)
         device,
         0, NULL);
 
-    cl_program greyscale_program;
-    compile_source(&greyscale_source_path, &greyscale_program, device, context);
-    cl_kernel greyscale_kernel = clCreateKernel(greyscale_program, "memset", NULL);
-
-    cl_program guidedFilter_program;
-    compile_source(&guidedFilter_source_path, &guidedFilter_program, device, context);
-    cl_kernel guidedFilter_kernel = clCreateKernel( guidedFilter_program, "memset", NULL );
 
     cl_program cost_by_layer_program;
     compile_source(&cost_by_layer_source_path, &cost_by_layer_program, device, context);
     cl_kernel cost_by_layer_kernel = clCreateKernel (cost_by_layer_program, "memset", NULL);
 
-    cv::Mat left_image;
-    to_greyscale_plus_padding(&left_image_path ,left_image  ,MAX_DISTANCE ,context, greyscale_kernel, queue, true);
-    cv::Mat right_image;
-    to_greyscale_plus_padding(&right_image_path,right_image ,MAX_DISTANCE, context, greyscale_kernel, queue, false);
-    guidedFilter(&left_image_path ,left_image, MAX_DISTANCE, context, guidedFilter_kernel, queue, true);
 
     //--------------------------------------------
     // Layer cost computation
-    int disparity_test =  1;
-    int padding_size = MAX_DISTANCE;
-    int number_rows_img = left_image.rows;
-    int number_cols_img = left_image.cols;
-    int channel_for_left = 0;
-    int channel_for_right = 1;
-    float alpha_weight = 0.5;
+    opencl_stuff ocl_stuff;
+    ocl_stuff.device = device;
+    ocl_stuff.context = context;
+    ocl_stuff.queue = queue;
 
-    // - Padding
-    cv::Rect extract_zone (padding_size, padding_size, number_cols_img, number_rows_img);
-    cv::Mat left_image_padded = cv::Mat::zeros(left_image.rows + 2*padding_size, left_image.cols + 2 * padding_size, CV_8UC1);
-    left_image.copyTo(left_image_padded(extract_zone));
-    cv::Mat right_image_padded  = cv::Mat::zeros(left_image.rows + 2*padding_size, left_image.cols + 2 * padding_size, CV_8UC1);
-    right_image.copyTo(right_image_padded(extract_zone));
-    cv::Mat output_layer_cost = cv::Mat::zeros(left_image.size(), CV_32FC1); // float
+    opencl_buffer cost_layer = cost_by_layer(left_image_path, right_image_path, 25, ocl_stuff);
+    cost_layer.write_img("Cost_for_layer_20.png", ocl_stuff);
 
-    // - Merging into a single matrix with entrelacement
-    cv::Mat source_images_padded;
-    cv::Mat temp_array[2]  = {left_image_padded, right_image_padded};
-    cv::merge(temp_array, 2, source_images_padded);
-
-    // - Allocating the buffers
-    cl_mem cost_input_buffer = clCreateBuffer(context,
-                                          CL_MEM_COPY_HOST_PTR | CL_MEM_READ_ONLY,
-                                          source_images_padded.total() * source_images_padded.elemSize(),
-                                          (void*)source_images_padded.data, NULL);
-    cl_mem cost_output_buffer = clCreateBuffer(context,
-                                        CL_MEM_WRITE_ONLY,
-                                        output_layer_cost.total() * output_layer_cost.elemSize(),
-                                        NULL, NULL);
-    // - Passing arguments to the kernel
-    clSetKernelArg(cost_by_layer_kernel, 0, sizeof(cost_input_buffer), (void*)&cost_input_buffer);
-    clSetKernelArg(cost_by_layer_kernel, 1, sizeof(cost_output_buffer), (void*)&cost_output_buffer);
-    clSetKernelArg(cost_by_layer_kernel, 2, sizeof(padding_size), (void*)&padding_size);
-    clSetKernelArg(cost_by_layer_kernel, 3, sizeof(disparity_test), (void*)&disparity_test);
-    clSetKernelArg(cost_by_layer_kernel, 4, sizeof(alpha_weight), (void*)&alpha_weight);
-    // - Enqueuing kernel
-    size_t global_work_size_cost_layer[] = {right_image.total() };
-    clEnqueueNDRangeKernel(queue,
-                           cost_by_layer_kernel,
-                           2,
-                           NULL,
-                           global_work_size_cost_layer,
-                           NULL,
-                           0,
-                           NULL, NULL);
-    // - Waiting end execution
-    clFinish(queue);
-    cout<<"dinisf"<<endl;
-//
-//    //--------------------------------------------
-//    //--------Difference Image Kernel-------------
-//    //--------------------------------------------
-//    //now source image == the greysclae image
-//
-//    cl_program difference_image_program;
-//    compile_source(&difference_image_source_path, &difference_image_program, device, context);
-//
-//    cl_kernel difference_image_kernel = clCreateKernel(difference_image_program, "memset", NULL);
-//
-//    cv::Mat output_image; // each image will be next to each other?
-//    image_difference(left_image, right_image, output_image, MAX_DISTANCE, context, difference_image_kernel, queue, false);
-
-    left_image.release();
-    right_image.release();
-//    output_image.release();
     return 0;
 }
